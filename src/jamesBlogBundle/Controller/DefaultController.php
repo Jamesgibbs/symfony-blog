f9a73a8f5550cebbb4541c746000ba9d76f582f3
james gibbs <jamesgibbbs@gmail.com>
2016-08-03 12:44:06 +0100

    second commit

--- src/jamesBlogBundle/Controller/DefaultController.php
+++ src/jamesBlogBundle/Controller/DefaultController.php
@@ -1,12 +1,18 @@
 <?php
-
 namespace jamesBlogBundle\Controller;
 
+use jamesBlogBundle\Entity\Task;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+
+
+	public function indexAction()
     {
         return $this->render('jamesBlogBundle:Default:index.html.twig');
     }
@@ -14,5 +20,23 @@ class DefaultController extends Controller
     public function page2Action()
     {
         return $this->render('jamesBlogBundle:Default:page2.html.twig');
+    }
+
+    public function newAction(Request $request)
+    {
+        // create a task and give it some dummy data for this example
+        $task = new Task();
+        $task->setTask('Write a blog post');
+        $task->setDueDate(new \DateTime('tomorrow'));
+
+        $form = $this->createFormBuilder($task)
+            ->add('task', TextType::class)
+            ->add('dueDate', DateType::class)
+            ->add('save', SubmitType::class, array('label' => 'Create Task'))
+            ->getForm();
+
+        return $this->render('jamesBlogBundle:Default:blog.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 }
